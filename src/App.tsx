336280f2b2f82691c22e6144be0a0f6e3e638956
Schengatto <schengatto@gmail.com>
2023-04-12 18:09:47 +0200

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { ShopPage } from './pages/shop/ShopPage'
+import { CheckOutPage } from './pages/checkout/CheckOutPage'
+import { CartPage } from './pages/cart/CartPage'
+import { ThanksPage } from './pages/checkout/ThanksPage'
+import { LoginPage } from './pages/login/LoginPage'
+import { CMSPage } from './pages/cms/CMSPage'
+import { CMSProductsPage } from './pages/cms/products/CMSProductsPage'
+import { CMSOrdersPage } from './pages/cms/orders/CMSOrdersPage'
 
 function App() {
 
   return (
-    <div className="page">
-      <ShopPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="shop" element={<ShopPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="checkout" element={<CheckOutPage />} />
+        <Route path="thanks" element={<ThanksPage />} />
+
+        <Route path="cms" element={<CMSPage />}>
+          <Route path="products" element={<CMSProductsPage />} />
+          <Route path="orders" element={<CMSOrdersPage />} />
+          <Route index element={<Navigate to="products" />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="shops" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,8 @@
+export const LoginPage: React.FunctionComponent = () => (
+    <div>
+        <h1 className="title">LoginPage</h1>
+
+        content here...
+    </div>
+
+);
